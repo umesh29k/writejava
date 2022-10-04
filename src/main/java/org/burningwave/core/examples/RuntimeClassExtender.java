b3dc529b481790f23cd0a04718c34d357c32a4f2
Kumar Singh, Umesh <ukumarsingh-consultant@scholastic.com>
2022-10-04 19:11:45 +0530

    Updates

---
+++ src/main/java/org/burningwave/core/examples/RuntimeClassExtender.java
@@ -0,0 +1,96 @@
+package org.burningwave.core.examples;
+
+import static org.burningwave.core.assembler.StaticComponentContainer.Constructors;
+import java.lang.reflect.Modifier;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
+import org.burningwave.core.Virtual;
+import org.burningwave.core.assembler.ComponentContainer;
+import org.burningwave.core.assembler.ComponentSupplier;
+import org.burningwave.core.classes.AnnotationSourceGenerator;
+import org.burningwave.core.classes.ClassFactory;
+import org.burningwave.core.classes.ClassSourceGenerator;
+import org.burningwave.core.classes.FunctionSourceGenerator;
+import org.burningwave.core.classes.GenericSourceGenerator;
+import org.burningwave.core.classes.TypeDeclarationSourceGenerator;
+import org.burningwave.core.classes.UnitSourceGenerator;
+import org.burningwave.core.classes.VariableSourceGenerator;
+
+public class RuntimeClassExtender {
+    @SuppressWarnings("resource")
+    public static void execute() throws Throwable {
+        UnitSourceGenerator unitSG = UnitSourceGenerator.create("packagename").addClass(
+                ClassSourceGenerator.create(
+                        TypeDeclarationSourceGenerator.create("MyExtendedClass")
+
+                ).addModifier(
+
+                        Modifier.PUBLIC
+
+                        //generating new method that override MyInterface.convert(LocalDateTime)
+                ).addMethod(
+                        FunctionSourceGenerator.create("convert").setReturnType(
+                                TypeDeclarationSourceGenerator.create(
+                                        Comparable.class
+                                ).addGeneric(
+                                        GenericSourceGenerator.create(Date.class)
+                                )
+                        )
+                                .addParameter(VariableSourceGenerator.create(LocalDateTime.class, "localDateTime"))
+                                .addModifier(Modifier.PUBLIC)
+                                .addAnnotation(AnnotationSourceGenerator.create(Override.class))
+                                .addBodyCode(
+                                        "return Date.from(localDateTime.atZone(ZoneId.systemDefault()).toInstant());"
+                                ).useType(ZoneId.class)
+                ).addConcretizedType(
+                        MyInterface.class
+                ).expands(ToBeExtended.class)
+        );
+        System.out.println("\nGenerated code:\n" + unitSG.make());
+        //With this we store the generated source to a path
+        unitSG.storeToClassPath(System.getProperty("user.home") + "/Desktop/bw-tests");
+        ComponentSupplier componentSupplier = ComponentContainer.getInstance();
+        ClassFactory classFactory = componentSupplier.getClassFactory();
+        //this method compile all compilation units and upload the generated classes to default
+        //class loader declared with property "class-factory.default-class-loader" in
+        //burningwave.properties file (see "Overview and configuration").
+        //If you need to upload the class to another class loader use
+        //loadOrBuildAndDefine(LoadOrBuildAndDefineConfig) method
+        Class<?> generatedClass = classFactory.loadOrBuildAndDefine(
+                unitSG
+        ).get(
+                "packagename.MyExtendedClass"
+        );
+        ToBeExtended generatedClassObject =
+                Constructors.newInstanceOf(generatedClass);
+        generatedClassObject.printSomeThing();
+        System.out.println(
+                ((MyInterface) generatedClassObject).convert(LocalDateTime.now()).toString()
+        );
+        //You can also invoke methods by casting to Virtual (an interface offered by the
+        //library for faciliate use of runtime generated classes)
+        Virtual virtualObject = (Virtual) generatedClassObject;
+        //Invoke by using reflection
+        virtualObject.invoke("printSomeThing");
+        //Invoke by using MethodHandle
+        virtualObject.invokeDirect("printSomeThing");
+        System.out.println(
+                ((Date) virtualObject.invokeDirect("convert", LocalDateTime.now())).toString()
+        );
+    }
+
+    public static class ToBeExtended {
+        public void printSomeThing() {
+            System.out.println("Called method printSomeThing");
+        }
+    }
+
+    public static interface MyInterface {
+        public Comparable<Date> convert(LocalDateTime localDateTime);
+    }
+
+    public static void main(String[] args) throws Throwable {
+        execute();
+    }
+}
